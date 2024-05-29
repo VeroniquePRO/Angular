4dff6c00e974dd76bc0d9d6450e1d6338cd8cfc2
Vero <vp.prosper@gmail.com>
2024-05-29 12:56:30 +0200

    Challenge 2 Angular

---
+++ src/app/block-component/block-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-component',
+  standalone: true,
+  imports: [],
+  template:`<div class="block">A B C</div>`,
+  styleUrl: './block-component.component.css'
+})
+export class BlockComponentComponent {
+
+}
---
+++ src/app/validation-button/validation-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validation-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './validation-button.component.html',
+  styleUrl: './validation-button.component.css'
+})
+export class ValidationButtonComponent {
+
+}
